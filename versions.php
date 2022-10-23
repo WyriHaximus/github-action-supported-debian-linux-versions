05d0b5bc5b6733021a0776191bf8b3b3873cd7df
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-10-23 23:56:55 +0200

    Use new way of outputting values from step/action
    Refs: https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- versions.php
+++ versions.php
@@ -7,4 +7,4 @@ $versions[] = 'buster';
 $versions[] = 'stretch';
 
 echo 'Found the following supported versions: ', implode(', ', $versions), PHP_EOL;
-echo '::set-output name=versions::', json_encode($versions), PHP_EOL;
+file_put_contents(getenv('GITHUB_OUTPUT'), 'versions=' . json_encode($versions) . PHP_EOL, FILE_APPEND);
